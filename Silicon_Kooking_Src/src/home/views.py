4abe40f88778c849ba087163ffb153fcea2b3f24
Saif Aljashamy <saljashamy@tuta.io>
2018-02-16 01:01:12 -0800

    Link home page to urls
    - Add syntax for templating.

--- Silicon_Kooking_Src/src/home/views.py
+++ Silicon_Kooking_Src/src/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import render_to_response
+
 
 def home_display(request):
-    return render(request, 'header.html', {})
+	return render(request, 'home/index.html', {})
