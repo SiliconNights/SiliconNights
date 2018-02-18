0a7586da4ecda1cd2bb4edf6feec60c3e462326f
Saif Aljashamy <saljashamy@tuta.io>
2018-02-17 23:58:05 -0800

    Add parsing for individual ingredients and tags
    - Now each recipe has its own ingredient list, including only the name of ingredients.
    - Each recipe has tags, that include style and nationality of dish.
    - Clean up useless data.

--- Silicon_Kooking_Src/models/data/clean-parse-data/generateXML.py
+++ Silicon_Kooking_Src/models/data/clean-parse-data/generateXML.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 # inserts text in between first and last 
-def insert_between(str, first, last):
+def get_between(str, first, last):
 	try:
 		start = str.index(first) + len(first)
 		end = str.index(last, start)
@@ -20,87 +20,140 @@ with open('datadump.xml', 'r', encoding='utf-8') as file:
  
 # get root element
 wikimedia = tree.getroot()
-
-f.write('<?xml version="1.0" ?>')
-f.write('\n<data>')
+count = 1
 string = ' '
+
+f.write('<?xml version="1.0" encoding="utf-8"?>')
+f.write('\n<data>')
+
 # iterate page items
 for page in wikimedia:
 	# iterate elements of page
 	for element in page:
 		# if title, store title
 		if element.tag == 'title':
-			string = element.text
+			title = element.text
 		# if revision
 		if element.tag == 'revision':
 			for child in element:
-				# insert text tag
+				# get text tag
 				if child.tag == 'text':
 					# check if contains recipe
 					if child.text != None and '== ingredients ==' in child.text.lower():
-							image = insert_between(child.text, '== Description ==', '== Ingredients ==')
-							if None != re.search(r'\[\[Image:(.*)\]\]', image): 
-								info = child.text
-								info = re.sub(r'<(.*)ref(.*)>', '', info)
-								info = re.sub(r'<(.*)br(.*)>', '', info)
-								info = info.replace('&','&amp;')
-								
+							# remove/fix misc. tags/data
+							info = child.text
+							info = re.sub(r'\[\[Image:(.*?)\]\]', '', info)
+							info = re.sub(r'\[\[File:(.*?)\]\]', '', info)
+							info = re.sub(r'\(\[\[Wikipedia:(.*?)\]\]\)', '', info)
+							info = info.replace('__NOTOC__', '')
+							info = info.replace('=== Other Links ===', '')
+							info = info.replace('== See also ==', '')												
+							info = re.sub(r'<(.*?)ref(.*?)>', '', info)
+							info = re.sub(r'<(.*?)br(.*?)>', '', info)
+							info = re.sub(r'<(.*?)span(.*?)>', '', info)
+							info = info.replace('&','&amp;')
+							info = re.sub(r'(\n)+','\n', info)
+							info = re.sub(r'^\=\n', '\n', info)
+							
+							# get ingredients
+							string = get_between(info,'== Ingredients ==', '== Directions ==')
+							string = re.sub(r'(\n)+$', '', string)
+							string = re.sub(r'^(\n)+', '', string)
+							ingredients = string
+							
+							# get list of ingredients
+							list = re.findall(r'\[\[(.*?)\]\]', string)
+							list = [item.lower() for item in list]
+							newlist = []
+							for i in list:
+								if i not in newlist:
+									newlist.append(i)
+							newlist.reverse()
+							list = newlist
+							
+							if len(list) > 0 :
 								# create new recipe
-								f.write('\n<recipe>')
+								tag = '\n<recipe number="' + str(count) + '">'
+								f.write(tag)
 								
 								# title
-								f.write('\n<title> ')
-								string = string.lstrip()
-								string = string.rstrip()
-								string = string.replace('\n\n','\n')
-								f.write(string)
+								f.write('\n<title>')
+								title = re.sub(r'(\n)+$', '', title)
+								title = re.sub(r'^(\n)+', '', title)
+								title = re.sub(r'(\n)+','\n', title)
+								title = title.strip()
+								f.write(title)
 								f.write('</title>')
-									
 								
 								# description
-								string = insert_between(info, '== Description ==', '== Ingredients ==')
+								string = get_between(info, '== Description ==', '== Ingredients ==')
 								f.write('\n<description>\n')
-								image = re.search(r'\[\[Image:(.*)\]\]', string)
-								string = re.sub(r'\[\[Image:(.*)\]\]', '', string)
-								string = string.lstrip()
-								string = string.rstrip()
-								string = string.replace('\n\n','\n')
-								string = string.replace('=', '')
-								string = string.lstrip()
-								string = string.rstrip()
-								f.write(string)
-								f.write('\n</description>')
-								
-								# image 
-								f.write('\n<image>')
-								f.write(image.group())
-								f.write('</image>')
-								
-									
+								string = string.replace('.', '')
+								string = re.sub(r'^= ', '', string)
+								string = re.sub(r'(\n)+$', '', string)
+								string = re.sub(r'^(\n)+', '', string)
+								string = string.strip()
+								if len(string) == 0:
+									f.write('none')
+								else:
+									f.write(string)
+								f.write('\n</description>\n')
+													
+													
 								# ingredients 
-								string = insert_between(info,'== Ingredients ==', '== Directions ==')
-								f.write('\n<ingredients>\n')
-								string = string.replace('=', '')
-								string = string.lstrip()
-								string = string.rstrip()
-								string = string.replace('\n\n','\n')
-								f.write(string)
-								f.write('\n</ingredients>')
+								f.write('<ingredients>\n')
+								f.write(ingredients)
+								f.write('\n</ingredients>\n')
+								
+								
+								# ingredient list
+								f.write('<ingredientList>\n')
+								for i in range(0, len(list)-1):
+									f.write(list[i])
+									f.write(', ')
+								f.write(list[len(list)-1])
+								f.write('\n</ingredientList>\n')
 									
-								# directions
-								string = insert_between(info,'== Directions ==', '[[Category')
-								f.write('\n<directions>\n')
-								string = string.lstrip()
-								string = string.rstrip()
-								string = string.replace('\n\n','\n')
-								string = string.replace('== Other Links ==', '')
-								string = string.replace('== See also ==', '')
-								string = string.replace('__NOTOC__', '')
-								string = string.replace('=', '')
-								f.write(string)
-								f.write('\n</directions>')
 									
+								# instructions
+								string = get_between(info,'== Directions ==', '[[Category')
+								f.write('<instructions>\n')
+								string = re.sub(r'(\n)+$', '', string)
+								string = re.sub(r'^(\n)+', '', string)
+								string = re.sub(r'(\n)+','\n', string)
+								string = string.strip()
+								f.write(string)
+								f.write('\n</instructions>\n')
+								
+								
+								# tags
+								f.write('<tags>\n')
+								list = re.findall(r'\[\[Category:(.*?)\]\]', info)
+								newlist = []
+								for item in list:
+									item = item.replace('Recipes', '')
+									item = item.strip()
+									item = item.lstrip('using ')
+									if item != 'that need photos' and item != 'with video instruction':
+										newlist.append(item)
+								list = newlist
+								newlist = []
+								for i in list:
+									if i not in newlist:
+										newlist.append(i)
+								newlist.reverse()
+								list = newlist
+								if len(list) == 0: 
+									f.write('none')
+								else:
+									for i in range(0, len(list)-1):
+										f.write(list[i])
+										f.write(', ')
+									f.write(list[len(list)-1])
+								f.write('\n</tags>')
 								f.write('\n</recipe>')
 								f.write('\n')
+								count = count + 1
 f.write('\n</data>')
 f.close()
+print(count)
