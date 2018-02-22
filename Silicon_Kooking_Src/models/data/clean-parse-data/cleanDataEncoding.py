9f820c36a8e7f52f9d7e9d098fe8d7606e5c78eb
Saif Aljashamy <saljashamy@tuta.io>
2018-02-21 23:32:28 -0800

    Add Windows encoding of data dump
    - Converts Windows-ISO encoding to Unicode equivalent.
    - Data should all be in Unicode.

--- Silicon_Kooking_Src/models/data/clean-parse-data/cleanDataEncoding.py
+++ Silicon_Kooking_Src/models/data/clean-parse-data/cleanDataEncoding.py
@@ -1,11 +1,13 @@
-# pip install ftfy
-from ftfy import fix_encoding
+from bs4 import UnicodeDammit
+from ftfy import fix_encoding, fix_text
 
 f = open('cleandata.xml', 'w', encoding='utf-8')
 
-# clean encoding
+# clean encoding 
 with open('datadump.xml', 'r', encoding='utf-8') as file:
 	for line in file:
 		line = fix_encoding(line)
-		f.write(line)
+		line = fix_text(line)
+		line = UnicodeDammit(line)
+		f.write(line.unicode_markup)
 f.close()
