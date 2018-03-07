69025460ef81063cc10f23393331a0015720e6cd
salinasr278 <rafaelsalinas262@gmail.com>
2018-03-06 23:26:10 -0800

    added url for get_recipes in views for autocomplete

--- Silicon_Kooking_Src/src/recipes/urls.py
+++ Silicon_Kooking_Src/src/recipes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>\d+)$', views.recipes_detail_display, name='recipes_detail_display'),
     url(r'^upload', views.upload_recipe, name='upload_recipe'),
+    url(r'^api/get_recipes/', views.get_recipes, name='get_recipes'),
 ]
