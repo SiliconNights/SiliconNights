d6d4a759ca0f9e2b4714eceeb3e03c02cbf09b19
BaldeSep <balde.sepu@protonmail.com>
2018-03-08 22:25:51 -0800

    new model created for user profile images

--- Silicon_Kooking_Src/src/accounts/admin.py
+++ Silicon_Kooking_Src/src/accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Favorite
+from .models import Favorite, ProfileImage
 
 # Register your models here.
 
 admin.site.register(Favorite)
+admin.site.register(ProfileImage)
--- Silicon_Kooking_Src/src/accounts/models.py
+++ Silicon_Kooking_Src/src/accounts/models.py
@@ -8,10 +8,16 @@ from recipes.models import Recipe
 class Favorite(models.Model):
     recipe = models.ForeignKey(Recipe, db_column='recipe', on_delete=models.PROTECT)
     user = models.ForeignKey(User, db_column='user', on_delete=models.PROTECT)
-	
+
     class Meta:
         managed = True
         db_table = 'favorite'
         unique_together = (('recipe', 'user'),)
 
+class ProfileImage(models.Model):
+    user = models.ForeignKey(User, db_column='user', on_delete=models.PROTECT, null=True)
+    avatar = models.ImageField(db_column="avatar", upload_to = 'profile-img/', default = 'profile-img/default.jpg', blank=True)
 
+    class Meta:
+        managed = True
+        db_table = 'profile_image'
