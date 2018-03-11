1f618431dd3dc010684d85e2df6bc431764b0fe1
BaldeSep <balde.sepu@protonmail.com>
2018-03-11 15:02:18 -0700

    Finished the search operation for new header

--- Silicon_Kooking_Src/src/recipes/admin.py
+++ Silicon_Kooking_Src/src/recipes/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Recipe, RecipesRecipe, Ingredient, IngredientRecipe, SimilarIngredient
+from .models import (
+    Recipe,
+    RecipesRecipe,
+    Ingredient,
+    IngredientRecipe,
+    SimilarIngredient,
+    Cuisine,
+    CuisineRecipe,
+    MealType,
+    MealTypeRecipe,
+)
 # Register your models here.
 
 admin.site.register(Recipe)
@@ -8,3 +18,7 @@ admin.site.register(Ingredient)
 admin.site.register(RecipesRecipe)
 admin.site.register(IngredientRecipe)
 admin.site.register(SimilarIngredient)
+admin.site.register(Cuisine)
+admin.site.register(CuisineRecipe)
+admin.site.register(MealType)
+admin.site.register(MealTypeRecipe)
